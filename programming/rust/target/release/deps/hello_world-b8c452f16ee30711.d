7d0a3e3b6345d37e4dea7fc9abae058314fe4820
hertz <hebin@smsz7931.sina.com>
2018-08-29 17:08:38 +0800

    add rust/ffm update doc

---
+++ programming/rust/target/release/deps/hello_world-b8c452f16ee30711.d
@@ -0,0 +1,5 @@
+/home/test_integration/programming/rust/target/release/deps/hello_world-b8c452f16ee30711: src/main.rs
+
+/home/test_integration/programming/rust/target/release/deps/hello_world-b8c452f16ee30711.d: src/main.rs
+
+src/main.rs:
---
+++ programming/rust/target/release/hello_world.d
@@ -0,0 +1,1 @@
+/home/test_integration/programming/rust/target/release/hello_world: /home/test_integration/programming/rust/src/main.rs
