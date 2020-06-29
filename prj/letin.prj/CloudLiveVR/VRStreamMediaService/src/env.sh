d21ebfec81c598beaf1f17434b80a14dd9c2d456
hertz <bin.he@letinvr.com>
2020-06-28 21:35:38 -0400

    backup system usage for nvidia driver

--- prj/letin.prj/CloudLiveVR/VRStreamMediaService/src/env.sh
+++ prj/letin.prj/CloudLiveVR/VRStreamMediaService/src/env.sh
@@ -6,6 +6,10 @@ export LD_LIBRARY_PATH=/home/hertz/sdb1/cerbero/build/dist/linux_x86_64/lib:/hom
 export GST_OMX_CONFIG_DIR=/home/hertz/.cache/cerbero-sources/gst-omx-1.0/config/bellagio/gstomx.conf
 export PKG_CONFIG_PATH=/home/hertz/sdb1/cerbero/build/dist/linux_x86_64/lib/pkgconfig/:/home/hertz/sdb1/Qt/Qt-5.13.2/5.13.2/gcc_64/lib/pkgconfig
 
-NVIDIA_DRIVER_VERSION=435.21
+#/proc/driver/nvidia/version
+NVIDIA_KERNEL_DRIVER_VERSION="NVRM version: NVIDIA UNIX x86_64 Kernel Module  435.21  Sun Aug 25 08:17:57 CDT 2019
+                      GCC version:  gcc version 7.5.0 (Ubuntu 7.5.0-3ubuntu1~18.04)"
+LINUX_KERNEL_VERSION="Linux version 5.3.0-56-generic (buildd@lcy01-amd64-017) (gcc version 7.5.0 (Ubuntu 7.5.0-3ubuntu1~18.04)) #50~18.04.1-Ubuntu SMP Wed May 27 08:52:12 UTC 2020"
 CUDA_VERSION=10.1
-NVIDIA-SMI=435.21
+NVIDIA_SMI=435.21
+NVIDIA_DRIVER_VERSION=435.21
