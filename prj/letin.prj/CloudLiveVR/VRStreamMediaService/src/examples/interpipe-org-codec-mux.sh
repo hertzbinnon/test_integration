8edc80b6e2709cb9c858b6149de0b056605b9fe7
hertz <bin.he@letinvr.com>
2021-01-26 21:16:42 -0500

    fix interpipe error: minimum latency bigger than maximum latency

--- prj/letin.prj/CloudLiveVR/VRStreamMediaService/src/examples/interpipe-org-codec-mux.sh
+++ prj/letin.prj/CloudLiveVR/VRStreamMediaService/src/examples/interpipe-org-codec-mux.sh
@@ -18,17 +18,17 @@ trap "STOP=1" SIGINT
 echo -e "\n ====> Create the scr_pipe_1 \n"
 gstd-client pipeline_create pipe_1_src uridecodebin uri=udp://192.168.0.134:12340 name=source \
 ! "video/x-raw, framerate=30/1, width=352, height=288, format=Y444_10LE" ! queue ! interpipesink name=src_1 caps=video/x-raw,width=352,height=288,framerate=30/1 sync=false async=false \
- source. ! queue ! audioconvert ! voaacenc ! aacparse ! queue ! interpipesink name=src_11 caps=audio/mpeg sync=false async=false
+                                                                 source. ! queue ! interpipesink name=src_11 caps=audio/mpeg sync=false async=false
 
 echo -e "\n ====> Create the scr_pipe_2 \n"
 gstd-client pipeline_create pipe_2_src uridecodebin uri=udp://192.168.0.134:12341 name=source \
 ! "video/x-raw, framerate=30/1, width=352, height=288, format=Y444_10LE" ! queue ! interpipesink name=src_2 caps=video/x-raw,width=352,height=288,framerate=30/1 sync=false async=false \
- source. ! queue ! audioconvert ! voaacenc ! aacparse ! queue ! interpipesink name=src_22 caps=audio/mpeg sync=false async=false
+                                                                 source. ! queue ! interpipesink name=src_22 caps=audio/mpeg sync=false async=false
 
 echo -e "\n ====> Create the scr_pipe_3 \n"
 gstd-client pipeline_create pipe_3_src uridecodebin uri=udp://192.168.0.134:12342 name=source \
 ! "video/x-raw, framerate=30/1, width=352, height=288, format=Y444_10LE" ! queue ! interpipesink name=src_3 caps=video/x-raw,width=352,height=288,framerate=30/1 sync=false async=false \
- source. ! queue ! audioconvert ! voaacenc ! aacparse ! queue ! interpipesink name=src_33 caps=audio/mpeg sync=false async=false
+                                                                 source. ! queue ! interpipesink name=src_33 caps=audio/mpeg sync=false async=false
 
 #echo -e "\n ====> Create the scr_pipe_2 \n"
 #gstd-client pipeline_create pipe_2_src videotestsrc pattern=snow is-live=true \
@@ -62,12 +62,12 @@ echo -e "\n ====> Create the sink_pipe_4 (listener) \n"
 #gstd-client pipeline_create pipe_4_sink interpipesrc name=interpipesrc1 listen-to=src_1 is-live=true allow-renegotiation=true ! videoconvert ! x264enc ! h264parse ! queue ! flvmux name=muxer ! rtmp2sink location=rtmp://192.168.0.134/live/chan0 sync=false interpipesrc name=interpipesrc11 listen-to=src_11 is-live=true allow-renegotiation=true ! audioconvert ! voaacenc ! aacparse ! queue ! muxer.
 
 gstd-client pipeline_create pipe_4_sink flvmux name=muxer ! rtmp2sink location=rtmp://192.168.0.134/live/ch5 sync=false \
-	interpipesrc name=interpipesrc4v listen-to=src_1  is-live=true allow-renegotiation=true format=3 stream-sync=2 max-latency=0 min-latency=0 ! queue ! videoconvert ! x264enc key-int-max=30 ! h264parse !  muxer. \
-	interpipesrc name=interpipesrc4a listen-to=src_11 is-live=true allow-renegotiation=true format=3 stream-sync=2 max-latency=0 min-latency=0 ! queue ! aacparse ! muxer.
+  interpipesrc name=interpipesrc4v listen-to=src_1 is-live=true allow-renegotiation=true format=3 stream-sync=2 max-latency=0 min-latency=0 ! queue ! videoconvert ! x264enc key-int-max=30 ! h264parse ! muxer. \
+  interpipesrc name=interpipesrc4a listen-to=src_11 is-live=true allow-renegotiation=true format=3 stream-sync=2 max-latency=0 min-latency=0 ! queue ! audioconvert ! voaacenc ! aacparse ! queue ! muxer.
 
 gstd-client pipeline_create pipe_5_sink flvmux name=muxer ! rtmp2sink location=rtmp://192.168.0.134/live/ch6 sync=false \
-	interpipesrc name=interpipesrc5v listen-to=src_1  is-live=true allow-renegotiation=true format=3 stream-sync=2 max-latency=0 min-latency=0 ! queue ! videoconvert ! x264enc key-int-max=30 ! h264parse !  muxer. \
-	interpipesrc name=interpipesrc5a listen-to=src_11 is-live=true allow-renegotiation=true format=3 stream-sync=2 max-latency=0 min-latency=0 ! queue ! aacparse ! muxer.
+  interpipesrc name=interpipesrc5v listen-to=src_1 is-live=true allow-renegotiation=true format=3 stream-sync=2 max-latency=0 min-latency=0 ! queue ! videoconvert ! x264enc key-int-max=30 ! h264parse ! muxer. \
+  interpipesrc name=interpipesrc5a listen-to=src_11 is-live=true allow-renegotiation=true format=3 stream-sync=2 max-latency=0 min-latency=0 ! queue ! audioconvert ! voaacenc ! aacparse ! queue ! muxer.
 # Change pipelines to PLAYING STATE
 echo -e "\n ====> Change to PLAYING STATE \n"
 gstd-client pipeline_play pipe_1_src
@@ -81,7 +81,7 @@ gstd-client pipeline_play pipe_5_sink
 
 echo -e "\n ====> Every 3 seconds the sink_pipe will change the src_pipe that is listening to \n"
 echo -e "\n ====> Start listening to scr_pipe_1 \n"
-sleep 10
+sleep 30
 
 # Start alternating the source pipeline being listened
 while :
@@ -91,7 +91,7 @@ do
 	gstd-client element_set pipe_5_sink interpipesrc5v listen-to src_2
 	gstd-client element_set pipe_5_sink interpipesrc5a listen-to src_22
 	echo -e "\n ====> Change to listening to scr_pipe_2 \n"
-	sleep 10.04
+	sleep 1.04
 	if [ $STOP -ne 0 ]
 	then
 		break
@@ -102,7 +102,7 @@ do
 	gstd-client element_set pipe_5_sink interpipesrc5v listen-to src_3
 	gstd-client element_set pipe_5_sink interpipesrc5a listen-to src_33
 	echo -e "\n ====> Change to listening to scr_pipe_3 \n"
-	sleep 0.04
+	sleep 1.04
 	if [ $STOP -ne 0 ]
 	then
 		break
@@ -114,7 +114,7 @@ do
 	gstd-client element_set pipe_5_sink interpipesrc5a listen-to src_11
 	echo -e "\n ====> Change to listening to scr_pipe_1 \n"
 	echo -e "\n ====> Type Ctrl+C to stop the example execution, otherwise it will iterate infinitely!\n"
-	sleep 0.04
+	sleep 1.04
 	if [ $STOP -ne 0 ]
 	then
 		break
