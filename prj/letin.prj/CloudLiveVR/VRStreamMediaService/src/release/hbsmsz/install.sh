8f2c7393c0c6a2f2c9d224d88eb75e80ae3b8cae
hertz <bin.he@letinvr.com>
2021-07-15 01:08:30 -0400

    add Director for new version

---
+++ prj/letin.prj/CloudLiveVR/VRStreamMediaService/src/release/hbsmsz/install.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+if [ $# != 1 ];then
+	echo "$0 <dir>"
+	exit 11
+fi
+INSTALL_PATH=$1
+VERSION=1.0.0-Ubuntu1804
+SYSVERSION="18.04"
+HDWVERSION="TeslaT4"
+
+sudo apt install ntpdate lsb-release libx11-xcb1 libjbig0 libegl1 -y 
+SYS=`lsb_release -a 2> /dev/null | grep Release | cut -d : -f2 | awk '{gsub(/^\s+|\s+$/, "");print}'`
+HDW=`nvidia-smi -q | grep "Product Name" | cut -d : -f2 | uniq |  awk '{gsub(/^\s+|\s+$/, "");print}' | sed s/[[:space:]]//g`
+if [ $SYS != $SYSVERSION ]; then
+  echo "$SYS not supported !!!"
+  exit 1
+fi
+if [ $HDW != $HDWVERSION ]; then
+  echo "$HDW not supported !!!"
+  exit 2
+fi
+#sudo apt install ntpdate -y
+if [ $? != 0 ];then
+	echo "Please check network !!! "
+       	exit 3
+fi
+sudo ntpdate ntp.ntsc.ac.cn
+if [ $? != 0 ];then
+	echo "sync date failed !!! "
+       	exit 4
+fi
+Exp_time=1631851200
+let exp_time=$Exp_time
+let elps_time=`date "+%s"`
+
+#echo "==$elps_time"
+#echo "==$exp_time"
+
+if [ $exp_time -lt $elps_time ];then
+	echo "out of time !!!"
+	exit 5
+fi
+
+cd $INSTALL_PATH
+export LD_LIBRARY_PATH=/usr/local/lib64:/usr/local/lib
+sudo rm -rf /usr/local/lib64 /var/local/vrsmsz /etc/vrsmsd.conf /usr/local/bin/* /usr/local/nginx
+sudo mkdir -p /usr/local/lib64
+sudo rm -rf /usr/bin/pip  /usr/bin/virtualenv 
+sudo cp -rf vrsmsz/Python-3.7.5/* /usr/local/
+#sudo apt-get remove --purge python3 python3-pip -y
+#sudo apt install python3 libx11-xcb1 libjbig0 -y
+#sudo apt install libx11-xcb1 libjbig0 -y
+sudo update-alternatives --install /usr/bin/python python /usr/local/bin/python3 3
+sudo apt install python3-pip -y
+sudo ln -s /usr/bin/pip3 /usr/local/bin/pip
+#pip install virtualenv
+#sudo ln -s /home/$USER/.local/bin/virtualenv /usr/bin/virtualenv
+sudo mkdir -p /var/local/vrsmsz
+sudo chown -R $USER.$USER /var/local/vrsmsz
+#virtualenv --python=python /var/local/vrsmsz/env
+pyvenv-3.7 /var/local/vrsmsz/env
+#cd $INSTALL_PATH
+cp -rf vrsmsz/django-master /var/local/vrsmsz/env
+cp -rf vrsmsz/uploadmodule /var/local/vrsmsz/env
+cd /var/local/vrsmsz/env
+. ./bin/activate
+pip install -e django-master
+deactivate
+cd - > /dev/null 2>&1
+cp -rf bin/*.jpg /var/local/vrsmsz
+sudo cp -f bin/*.conf /etc
+sudo cp -rf bin/* /usr/local/bin/
+sudo cp -rf linux_x86_64/lib/* /usr/local/lib64
+sudo cp -rf  vrsmsz/nginx /usr/local/
+sudo chown -R $USER.$USER /usr/local/nginx
+sudo mkdir /var/log/vrsmsz /var/run/vrsmsz
+rm -rf $INSTALL_PATH/
+rm -rf ~/.cache/gstreamer-1.0/registry.x86_64.bin
+exit 0;
---
+++ prj/letin.prj/CloudLiveVR/VRStreamMediaService/src/release/hbsmsz/package.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#VERSION=`grep VERSION install.sh | head -1`
+#VERSION=`echo $VERSION | cut -d \= -f2`
+#PACKAGENAME=director-$VERSION-setup.bin
+
+PACKAGENAME=Cloud-Director-1.2.1-setup.bin
+ARCHIVEFILE=director.tar.gz
+DIRECTORY=./linux_x86_64
+. Expiration
+cd  program
+../shc -e $DAY/$MONTH/$YEAR -r -f _daemon
+../shc -e $DAY/$MONTH/$YEAR -r -f director 
+cp -f _daemon.x ../bin/_daemon
+cp -f director.x ../bin/director
+gcc -o vrsync -DYEAR=$YEAR -DMONTH=$MONTH -DDAY=$DAY ntptime.c
+cp -f vrsync ../bin/vrsync
+cd ..
+Exp_time=`date -d "$YEAR-$MONTH-$DAY 00:00:00" +"%s"`
+sed -i "/Exp_time=/cExp_time=$Exp_time" install.sh
+./shc -e $DAY/$MONTH/$YEAR -r -f install.sh 
+mv install.sh.x install.bin
+tar zcf $ARCHIVEFILE install.bin bin ../vrsmsz/django-master ../vrsmsz/nginx ../vrsmsz/uploadmodule $DIRECTORY/lib ../vrsmsz/Python-3.7.5 #python-3.7.7
+cat pre-install.sh $ARCHIVEFILE  > $PACKAGENAME
+rm -rf $ARCHIVEFILE install.bin install.sh.x.c 
+chmod a+x $PACKAGENAME
