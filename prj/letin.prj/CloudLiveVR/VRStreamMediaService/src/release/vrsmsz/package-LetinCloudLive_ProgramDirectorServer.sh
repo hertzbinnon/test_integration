9600bbf9c88c66d4e2fdb5b0342dfc2fa8f67b48
hertz <bin.he@letinvr.com>
2021-09-03 04:53:50 -0400

    fov and 重磅

--- prj/letin.prj/CloudLiveVR/VRStreamMediaService/src/release/vrsmsz/package-LetinCloudLive_ProgramDirectorServer.sh
+++ prj/letin.prj/CloudLiveVR/VRStreamMediaService/src/release/vrsmsz/package-LetinCloudLive_ProgramDirectorServer.sh
@@ -3,15 +3,15 @@
 #VERSION=`echo $VERSION | cut -d \= -f2`
 #PACKAGENAME=director-$VERSION-setup.bin
 
-PACKAGENAME=Cloud-Director-1.2.0-setup.bin
+PACKAGENAME=Cloud-Director-1.2.1-setup.bin
 ARCHIVEFILE=director.tar.gz
 DIRECTORY=~/sdb1/LetinCloudLive_ProgramDirectorServer/release/
 . Expiration
 rm $PACKAGENAME
 cd  program
-../shc -e $DAY/$MONTH/$YEAR -r -f _daemon
-../shc -e $DAY/$MONTH/$YEAR -r -f vrsmsd 
-../shc -e $DAY/$MONTH/$YEAR -r -f director 
+../shc -e $DAY/$MONTH/$YEAR -m "bin.he@letinvr.com" -U -r -f _daemon
+../shc -e $DAY/$MONTH/$YEAR -m "bin.he@letinvr.com" -U -r -f vrsmsd 
+../shc -e $DAY/$MONTH/$YEAR -m "bin.he@letinvr.com" -U -r -f director 
 cp -f _daemon.x ../bin/_daemon
 cp -f vrsmsd.x ../bin/vrsmsd
 cp -f director.x ../bin/director
@@ -20,7 +20,7 @@ cp -f vrsync ../bin/vrsync
 cd ..
 Exp_time=`date -d "$YEAR-$MONTH-$DAY 00:00:00" +"%s"`
 sed -i "/Exp_time=/cExp_time=$Exp_time" install.sh
-./shc -e $DAY/$MONTH/$YEAR -r -f install.sh 
+./shc -e $DAY/$MONTH/$YEAR -m "bin.he@letinvr.com" -U -r -f install.sh 
 mv install.sh.x install.bin
 #tar zcf $ARCHIVEFILE install.bin bin django-master nginx uploadmodule lib
 mv bin/_vrsmsz _vrsmsz
